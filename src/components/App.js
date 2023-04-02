b3ba8f68f0a1f2dc58aec63979c6c732e62f95a3
Faisal abrar <114504997+Fizz007@users.noreply.github.com>
2023-04-02 15:10:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
   useEffect(() => {
     setLoading(true);
     fetch(
-      `https://gnews.io/api/v4/top-headlines?category=$ab54bb805fe7a0b364d283a0004195a3&max=10&lang=en`
+      `https://gnews.io/api/v4/top-headlines?category=${category}&apikey=[API_KEY]&max=10&lang=en`
     )
       .then((res) => res.json())
       .then((data) => {
